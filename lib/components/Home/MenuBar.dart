4b2f13f7db1f352b0b44d409350d3afbf3f20f12
Jerry Kim <jerryrox@live.co.kr>
2020-07-08 15:39:27 +1200

    Fixed position issues

--- lib/components/Home/MenuBar.dart
+++ lib/components/Home/MenuBar.dart
@@ -16,48 +16,46 @@ class MenuBar extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Text(
-            // TODO: Change this to today's date
-            "Jul 8",
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 24.0
-            ),
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text(
+          // TODO: Change this to today's date
+          "Jul 8",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 24.0
           ),
-          Expanded(
-            flex: 1,
-            child: Container(),
+        ),
+        Expanded(
+          flex: 1,
+          child: Container(),
+        ),
+        IconButton(
+          icon: Icon(
+            Icons.calendar_today,
+            color: Colors.black,
+            size: 20.0
           ),
-          IconButton(
-            icon: Icon(
-              Icons.calendar_today,
-              color: Colors.black,
-              size: 20.0
-            ),
-            onPressed: _onCalendarButton,
+          onPressed: _onCalendarButton,
+        ),
+        IconButton(
+          icon: Icon(
+            Icons.add,
+            color: Colors.black,
+            size: 20.0
           ),
-          IconButton(
-            icon: Icon(
-              Icons.add,
-              color: Colors.black,
-              size: 20.0
-            ),
-            onPressed: _onAddButton,
+          onPressed: _onAddButton,
+        ),
+        IconButton(
+          icon: Icon(
+            Icons.more_horiz,
+            color: Colors.black,
+            size: 20.0
           ),
-          IconButton(
-            icon: Icon(
-              Icons.more_horiz,
-              color: Colors.black,
-              size: 20.0
-            ),
-            onPressed: _onMenuButton,
-          ),
-        ],
-      ),
+          onPressed: _onMenuButton,
+        ),
+      ],
     );
   }
 }
