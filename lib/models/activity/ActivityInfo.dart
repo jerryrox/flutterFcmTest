4d50218399872feab91a2f1a10ae98947ef7be6d
Jerry Kim <jerryrox@live.co.kr>
2020-07-08 21:08:24 +1200

    Added way to get user-friendly text for list of days

--- lib/models/activity/ActivityInfo.dart
+++ lib/models/activity/ActivityInfo.dart
@@ -6,4 +6,14 @@ class ActivityInfo {
   final List<DayType> days;
 
   ActivityInfo(this.name, this.days);
+
+  String getDaysString() {
+    String str = "";
+    for(int i=0; i<days.length; i++) {
+      if(i > 0)
+        str += ", ";
+      str += days[i].toString();
+    }
+    return str;
+  }
 }
