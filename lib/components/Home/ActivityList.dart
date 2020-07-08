09e4b6fd56e47a1f629466a3e80a72d57ca92a79
Jerry Kim <jerryrox@live.co.kr>
2020-07-08 21:09:06 +1200

    Draw ActivityCell using ActivityModel

--- lib/components/Home/ActivityList.dart
+++ lib/components/Home/ActivityList.dart
@@ -1,19 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutterFcmTest/components/Home/ActivityCell.dart';
+import 'package:flutterFcmTest/models/ActivityModel.dart';
+import 'package:provider/provider.dart';
 
 class ActivityList extends StatelessWidget {
-  
+
   @override
   Widget build(BuildContext context) {
+
+    final activityModel = Provider.of<ActivityModel>(context);
+
     return Expanded(
       flex: 1,
       child:Container(
-        child: ListView(
+        child: ListView.builder(
           scrollDirection: Axis.vertical,
-          children: [
-            ActivityCell(),
-            ActivityCell()
-          ],
+          itemCount: activityModel.activities.length,
+          itemBuilder: (context, index) {
+            return ActivityCell(
+              activity: activityModel.activities[index],
+            );
+          },
         ),
       )
     );
