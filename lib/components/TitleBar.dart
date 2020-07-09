456203a55161e475426a8cd56618c61f9d23dd3d
Jerry Kim <jerryrox@live.co.kr>
2020-07-09 15:35:25 +1200

    Added children parameter to TitleBar

--- lib/components/TitleBar.dart
+++ lib/components/TitleBar.dart
@@ -4,12 +4,12 @@ import 'package:flutterFcmTest/components/TitleText.dart';
 class TitleBar extends StatelessWidget {
 
   final String title;
-  final List<Widget> children;
+  final List<Widget> children = List<Widget>();
 
   TitleBar({
     Key key,
     @required this.title,
-    @required this.children
+    List<Widget> children,
   }) : super(key: key) {
     this.children.insert(0, TitleText(
       title
@@ -18,6 +18,10 @@ class TitleBar extends StatelessWidget {
       flex: 1,
       child: Container()
     ));
+
+    if(children != null) {
+      this.children.addAll(children);
+    }
   }
 
   @override
